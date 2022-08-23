8194830021c657011772dc4008685f604845565c
Ranjan <lamsalranjan19@gmail.com>
2022-08-23 15:35:08 +0545

    user post req handled

--- User/urls.py
+++ User/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.UserAPI.as_view(), name='UserAP'),
+    path('', views.UserAPI.as_view(), name='UserAPI'),
     path('<pk>', views.UserInfoAPI.as_view(), name='UserInfoAPI'),
     path('<pk>/followed', views.UserFollowedTagAPI.as_view(), name='UserFollowedTagAPI'),
 ]
--- User/views.py
+++ User/views.py
@@ -18,6 +18,13 @@ class UserAPI(APIView):
         serializer = UserSerializer(users, many=True)
         print(serializer.data)
         return Response(serializer.data)
+    
+    def post(self, request, pk):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 class UserInfoAPI(APIView):
     def get(self, request, pk):
