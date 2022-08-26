8161c099005dab95143e441cccd243cc5b6fedc0
Ranjan <lamsalranjan19@gmail.com>
2022-08-26 13:23:12 +0545

    password

--- User/models.py
+++ User/models.py
@@ -1,3 +1,4 @@
+from re import T
 from django.db import models
 from Tag.models import Tag
 # Create your models here.
@@ -6,9 +7,9 @@ class User(models.Model):
 
     name = models.CharField(max_length=120)
     username = models.CharField(max_length=120, null=False, blank=False)
-    email = models.EmailField(null=False, blank= False)
-    password = models.CharField(max_length=128, null=False, blank=False)
-    password2 = models.CharField(max_length=128, null=False, blank=False)
+    email = models.EmailField(null=True, blank= True)
+    password = models.CharField(max_length=128, null=True, blank=True)
+    password2 = models.CharField(max_length=128, null=True, blank=True)
     # photo = models.ImageField(null= True, blank=True)
     # followed = models.ManyToManyField(Tag, related_name="followed", default=None)
 
