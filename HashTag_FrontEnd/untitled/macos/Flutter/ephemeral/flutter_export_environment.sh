0af668cb32fc87ebdb76f2bb5daf06ddf6a6b1a2
5Asim <ashim.shrestha.55@gmail.com>
2023-05-11 12:25:10 +0545

    updated

---
+++ HashTag_FrontEnd/untitled/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\flutter\flutter_windows_2.10.4-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ACER\Desktop\project_4thsem\HashTag\HashTag_FrontEnd\untitled"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
---
+++ HashTag_FrontEnd/untitled/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter\\flutter_windows_2.10.4-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ACER\\Desktop\\project_4thsem\\HashTag\\HashTag_Backend\\HashTag\\HashTag_FrontEnd\\untitled" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter\\flutter_windows_2.10.4-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\ACER\\Desktop\\project_4thsem\\HashTag\\HashTag_Backend\\HashTag\\HashTag_FrontEnd\\untitled"
+  "FLUTTER_ROOT=C:\\flutter\\flutter_windows_2.10.4-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ACER\\Desktop\\project_4thsem\\HashTag\\HashTag_Backend\\HashTag\\HashTag_FrontEnd\\untitled\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ACER\\Desktop\\project_4thsem\\HashTag\\HashTag_Backend\\HashTag\\HashTag_FrontEnd\\untitled"
+  "FLUTTER_TARGET=C:\\Users\\ACER\\Desktop\\project_4thsem\\HashTag\\HashTag_Backend\\HashTag\\HashTag_FrontEnd\\untitled\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ACER\\Desktop\\project_4thsem\\HashTag\\HashTag_Backend\\HashTag\\HashTag_FrontEnd\\untitled\\.dart_tool\\package_config.json"
+)
